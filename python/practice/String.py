a3beb3fc175174b1211cbd412a147b95a092a661
ShreyanshKunjera <shreyanshkunjera@gmail.com>
2024-02-01 14:29:36 +0530

    Adding String.py file to practice folder

---
+++ python/practice/String.py
@@ -0,0 +1,24 @@
+s1="This is single line string"
+s2="""This is
+    Multiline
+    string"""
+
+print("String s1 is : ",s1)
+print("String s2 is : ",s2)
+
+print("Length of s1 is :",len(s1))
+print("Length of s2 is :",len(s2))
+
+print("Print s1 3 times :",s1*3)
+print("First char of s1 is :",s1[0])
+
+print(s1[0:5])
+print(s1[2:5])
+print(s1[:5])
+print(s1[2:])
+print(s1[-4:-1])
+print(s1[0:10:1])
+print(s1[0:10:2])
+print(s1[0:10:3])
+
+print("Reversed string is : ",s1[::-1])
