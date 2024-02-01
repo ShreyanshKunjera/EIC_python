09cc6dedd9a138e1aa958882b658a1c52d978b47
ShreyanshKunjera <shreyanshkunjera@gmail.com>
2024-02-01 14:30:27 +0530

    Adding numeric.py file to practice folder

---
+++ python/practice/numeric.py
@@ -0,0 +1,32 @@
+a=10
+b=37
+c=-567
+d=0.34
+print(a,b,c,d)
+print("Type of a is :",type(a))
+print("Type of c is :",type(c))
+print("Type of d is :",type(d))
+
+e=2+6j
+print(e)
+print("Type of e is :",type(e))
+
+f=0B1000
+print(f)
+print("Type of f is :",type(f))
+
+g=True
+print(g)
+print("Type of g is :",type(g))
+
+
+h=bytes(a)
+print(h)
+print("Type of h is :",type(h))
+
+i=float(a)
+print(i)
+print("Type of i is :",type(i))
+
+j=float("22.5")
+print(j)
