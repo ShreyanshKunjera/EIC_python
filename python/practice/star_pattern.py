5a8f92d34699c6dfc0ba78e7adbb37664f2612e1
ShreyanshKunjera <shreyanshkunjera@gmail.com>
2024-02-01 14:30:43 +0530

    Adding star_pattern.py file to practice folder

---
+++ python/practice/star_pattern.py
@@ -0,0 +1,11 @@
+line=int(input("Enter the number of lines you want to print the pattern: "))
+
+'''for i in range(1,line+1):
+	for j in range(1,i+1):
+		print("*",end="")
+	print()'''
+
+for i in range(1,line+1):
+	print("*"*i)
+
+
