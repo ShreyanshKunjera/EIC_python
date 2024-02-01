a3664f03eecd339a8d123f791a31b1a52a4bf623
ShreyanshKunjera <shreyanshkunjera@gmail.com>
2024-02-01 14:15:41 +0530

    Adding assignment1 to assignment folder

---
+++ python/assignments/assignment1.py
@@ -0,0 +1,14 @@
+First_name="Shreyansh"
+Last_name="Kunjera"
+age=21
+ssn=202003031
+Height=5.9
+Weight=65
+
+
+print("First name is : ",First_name)
+print("Last name is : ",Last_name)
+print("Age is : ",age)
+print("Ssn is : ",ssn)
+print("Height is : ",Height)
+print("Weight is : ",Weight)
