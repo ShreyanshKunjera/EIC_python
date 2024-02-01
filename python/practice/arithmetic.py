7c735277fa738858bcc7dcf29f69196ad72afb18
ShreyanshKunjera <shreyanshkunjera@gmail.com>
2024-02-01 14:31:37 +0530

    Adding arithmetic.py file to practice folder

---
+++ python/practice/arithmetic.py
@@ -0,0 +1,9 @@
+a=9
+b=7
+
+print("Addition is : ",a+b)
+print("Subtraction is : ",a-b)
+print("Multiplication is : ",a*b)
+print("Division is : ",a/b)
+print("Power is : ",a**b)
+print("Floor division is : ",a//b)
