b452c486c78de05f2f9b34fc1097740859bbebf3
ShreyanshKunjera <shreyanshkunjera@gmail.com>
2024-02-01 14:16:21 +0530

    Adding assignment4 to assignment folder

---
+++ python/assignments/assignment4.py
@@ -0,0 +1,9 @@
+# MyWay Sandwich
+
+print("Pick three of the below toppings: ")
+print("Onions\nLettuce\nTomato\nOlives\nPeppers")
+lst=input().split()
+print("How many sandwiches do you want?")
+num=int(input())
+total=num*5
+print("Your total is ",total,"$")
